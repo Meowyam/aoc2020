518ae0aae6adf447ce9dd12e83dbd921e62d8488
Meowyam <maryam@chilon.net>
2020-12-01 23:47:47 +0800

    day 1 part 2

--- day1.hs
+++ day1.hs
@@ -6,14 +6,28 @@ import Data.List
 convertInt :: [String] -> [Int]
 convertInt = map read
 
-search :: (Ord a, Num a) => [a] -> [a] -> a
-search (l:ls) (r:rs)
-  | ((l + r) < 2020) = search ls (r:rs)
-  | ((l + r) > 2020) = search (l:ls) rs
+searchTwo :: (Ord a, Num a) => [a] -> [a] -> a
+searchTwo (l:ls) (r:rs)
+  | ((l + r) < 2020) = searchTwo ls (r:rs)
+  | ((l + r) > 2020) = searchTwo (l:ls) rs
   | otherwise = (l * r)
+
+-- get possible combinations
+-- filter sums
+
+combiThree ls =
+  [[x, y, z] | x <- ls, y <- ls, z <- ls, x < y, y < z]
+
+filterThree ls =
+  foldr (*) 1 $ concat $ filter (\x -> sum x == 2020) ls
 
 main = do
   input <- fmap lines (readFile "input.txt")
   let format = sort $ convertInt input 
-  let results = search format (reverse format)
-  print results
+  let resultsTwo = searchTwo format (reverse format)
+  putStrLn "product of the two numbers"
+  print resultsTwo
+  putStrLn "product of the three numbers"
+  let resultsThree = filterThree (combiThree format)
+  print resultsThree
+
