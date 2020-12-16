ef292a1a800b40568a5e7afce76ef2afef5784eb
Meowyam <maryam@chilon.net>
2020-12-16 13:23:56 +0800

    Day 3

--- day3.hs
+++ day3.hs
@@ -1,15 +1,18 @@
 import Data.List
 
-move [] _ = []
-move (y:ys) x = (y !! i) : (move ys (x+3))
+move [] _ _ _ = []
+move (y:ys) x n s = (y !! i) : (move (drop s ys) (x+n) n s)
   where i = x `mod` (length y)
 
 count str c
   = length $ filter (==c) str
 
+slopes y x k [] = []
+slopes y x k (n:ns) =
+  (count (move y x n k) '#') : slopes y x k ns
+
 main = do
   input <- fmap lines (readFile "input3.txt")
-  -- how high
-  print $ length input
-  print $ move input 0
-  print $ count (move input 0) '#'
+  print $ move input 0 3 0
+  print $ count (move input 0 3 0) '#'
+  print $ foldr (*) 1 $ slopes input 0 0 [1,3,5,7] ++ slopes input 0 1 [1]
